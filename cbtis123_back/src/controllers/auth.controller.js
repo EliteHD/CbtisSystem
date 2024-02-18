94c6034f08b404d3f5d1f309f3bffdc698a627af
EliteHD <014424177@ulsaoaxaca.edu.mx>
2024-02-18 13:42:06 -0600

    login logic

--- cbtis123_back/src/controllers/auth.controller.js
+++ cbtis123_back/src/controllers/auth.controller.js
@@ -1,32 +1,97 @@
+const User = require('../models/user.model');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const register = async (req, res) => {
+    const { email, password, username } = req.body;
+
     try {
-        const { username, email, password } = req.body;
-        const user = new User({ username, email, password });
-        await user.save();
-        res.status(201).json({ message: 'User created successfully' });
-    } catch (error) {
-        res.status(500).json({ message: 'Error creating user' });
+        const passwordHash = await bcrypt.hash(password, 10);
+        const newUser = new User(
+            {
+                email,
+                password: passwordHash,
+                username
+            });
+        const savedUser = await newUser.save();
+        jwt.sign(
+            {
+                id: savedUser._id
+            },
+            process.env.JWT_SECRET || 'secretjos',
+            {
+                expiresIn: 3600
+            },
+            (err, token) => {
+                if (err) console.error(err);
+                res.cookie('token', token);
+
+                res.json(
+                    {
+                        token,
+                        id: savedUser._id,
+                        email: savedUser.email,
+                        username: savedUser.username,
+                        createdAt: savedUser.createdAt,
+                        updatedAt: savedUser.updatedAt
+                    }
+                )
+            }
+        )
+
+       
+    }
+    catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Error al registrar usuario' });
     }
 }
-
 const login = async (req, res) => {
+    const { email, password } = req.body;
     try {
-        const { email, password } = req.body;
         const user = await User
-            .findOne({ email })
-            .select('+password');   
+            .findOne({ email });
         if (!user) {
-            return res.status(404).json({ message: 'User not found' });
+            return res.status(400).json({ msg: 'Usuario no existe' });
         }
-        if (user.password !== password) {
-            return res.status(401).json({ message: 'Invalid password' });
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({ msg: 'Password invalidas' });
         }
-        res.status(200).json({ message: 'User logged in successfully' });
-    }
-    catch (error) {
-        res.status(500).json({ message: 'Error logging in' });
+        jwt.sign(
+            {
+                id: user._id
+            },
+            process
+                .env.JWT_SECRET || 'secretjos', {
+            expiresIn: 3600
+        },
+            (err, token) => {
+                if (err) throw err;
+                res.cookie('token', token);
+                res.json(
+                    {
+                        token,
+                        id: user._id,
+                        email: user.email,
+                        username: user.username,
+                        createdAt: user.createdAt,
+                        updatedAt: user.updatedAt
+                    }
+                )
+                
+            }
+        )   }
+    catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Error al loguear usuario' });
     }
 }
+const logout = (req, res) => { 
+    res.clearCookie('token');
+    res.json({ msg: 'Logout exitoso' });
+    return res.status(200);
+}
 
-module.exports = { register, login };
+module.exports = { register, login, logout };
+
--- cbtis123_back/src/routes/auth.routes.js
+++ cbtis123_back/src/routes/auth.routes.js
@@ -1,10 +1,12 @@
 const {Router} = require('express');
-const {login, register} = require('../controllers/auth.controller');
+const {login, register, logout} = require('../controllers/auth.controller');
 const router = Router();
 
-router.post('/signup', register);
+router.post('/register', register);
 
-router.post('/signin', login);
+router.post('/login', login);
+
+router.post('/logout', logout)
 
 module.exports = router;
 
